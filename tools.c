e8d0da33121bc320063c6f49b6746a3a8fdada89
Wilf Kieran <wilfkieran@eduroam-45-48.nomadic.bris.ac.uk>
2024-02-02 13:31:57 +0000

    says hi from wilf

--- tools.c
+++ tools.c
@@ -4,6 +4,8 @@ int main(){
 	printf("I am a tool and I am hard\n");
 	printf("GET EDITED SON");
         printf("Hello from the develop branch");
+        
+        printf("Hello from wilf's own personal branch");
         return 0;
 }
 
