5e2ac925d5c71552992901370f941c277737858e
Wilf Kieran <wilfkieran@eduroam-45-48.nomadic.bris.ac.uk>
2024-02-02 13:09:28 +0000

    Fixed it hopefully

--- tools.c
+++ tools.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 int main(){
-<<<<<<< HEAD
 	printf("I am a tool and I am hard\n");
-	return 0;
-=======
+	
 	printf("I am a tool and I am soft\n");
 	printf("GET EDITED SON");
         return 0;
->>>>>>> 97e5914b1c70eb4f18b87f243b211f2511c5d31e
+
 }
 
 
